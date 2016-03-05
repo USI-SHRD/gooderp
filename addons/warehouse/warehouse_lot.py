f66ddb814ba9155482c5b9b6eee56c1fd3561940
朱正翔 <feiyuc8@gmail.com>
2016-03-06 00:50:14 +0800

    添加正常编辑视图中通过点击按钮填充one2many值

--- addons/warehouse/warehouse_lot.py
+++ addons/warehouse/warehouse_lot.py
@@ -10,9 +10,9 @@ class wh_lot(osv.osv):
 
     _columns = {
         'name': fields.char(u'序列号'),
-        'line_id': fields.many2one('wh.move', u'库存调拨'),
-        'goods_id': fields.many2one('goods', u'产品'),
-        'warehouse_id': fields.many2one('warehouse', u'仓库'),
+        'line_id': fields.many2one('wh.move.line', u'库存调拨'),
+        'goods_id': fields.related('line_id', 'goods_id', type='many2one', relation='goods', string=u'产品'),
+        'warehouse_id': fields.related('line_id', 'warehouse_id', type='many2one', relation='warehouse', string=u'仓库'),
         'goods_qty': fields.float(u'数量', digits_compute=dp.get_precision('Goods Quantity')),
         'note': fields.text(u'备注'),
     }
--- extra/web_editable_open_dialog/static/src/js/dialog.js
+++ extra/web_editable_open_dialog/static/src/js/dialog.js
@@ -4,79 +4,73 @@ openerp.web_editable_open_dialog = function(instance) {
             var self = this,
                 res = this._super.apply(this, arguments);
 
-            console.log(self);
             return res.then(function() {
                 _.each(self.columns, function(column) {
-                    if (column.options && _.isString(column.options) && column.options.indexOf('set_one2many') != -1 && column.options.indexOf('open_dialog') != -1) {
-                        var dialog = self.$el.find('.oe_form_container [data-fieldname=' + column.id + '] a.open_dialog');
-                        if (dialog.length === 0) {
-                            dialog = $("<a class='open_dialog'>...</a>").click(function(e) {
-                                e.preventDefault();
-                                var options = instance.web.py_eval(column.options || '{}'),
-                                    notify = instance.web.notification,
-                                    pop = new instance.web.form.FormOpenPopup(self);
+                    if (column.options && _.isString(column.options) && column.options.indexOf('open_dialog') != -1) {
 
-                                console.warn('options', options);
-                                if (_.isUndefined(options.open_dialog.field)) {
-                                    field_column = column;
-                                } else {
-                                    field_column = _.find(self.columns, function(column) { return column.id === options.open_dialog.field});
-                                }
+                        if (column.options.indexOf('set_one2many') != -1) {
+                            var dialog = self.$el.find('.oe_form_container [data-fieldname=' + column.id + '] a.open_dialog');
+                            if (dialog.length === 0) {
+                                dialog = $("<a class='open_dialog'>...</a>").click(function(e) {
+                                    e.preventDefault();
+                                    self.do_open_one2many_popup(instance.web.py_eval(column.options || '{}'));
+                                });
 
-                                if (_.isUndefined(field_column)) {
-                                    return notify.warn('错误', options.open_dialog.field + '字段需要在视图中定义');
-                                }
+                                self.$el.find('.oe_form_container [data-fieldname=' + column.id + ']').append(dialog)
+                            };
+                        } else if (column.options.indexOf('set_one2many_readonly') != -1) {
 
-
-                                if (_.isUndefined(options.open_dialog.view_id)) {
-                                    return notify.warn('错误', 'options中需要定义view_id来指定具体的视图');
-                                }
-
-                                var views = options.open_dialog.view_id.split('.');
-                                if (views.length != 2) {
-                                    return notify.warn('错误', 'options中定义的视图id需要指定具体的模块名称');
-                                }
-
-                                new instance.web.Model('ir.model.data').call('get_object_reference', views).then(function(result) {
-                                    console.log('result', result);
-                                })
-
-                                pop.show_element(
-                                    'wh.move.line',
-                                    false,
-                                    self.dataset.get_context(),
-                                    {
-                                        title: '你好',
-                                        view_id: 288,
-                                        write_function: function(id, data, options) {
-                                            console.warn('write_function', id, data, options);
-
-                                            return $.Deferred();
-                                        },
-                                        create_function: function(data, options) {
-                                            console.warn('create_function', data, options);
-                                            return $.Deferred();
-                                        }
-                                    }
-                                );
-
-                                // pop.on('write_completed', self, function(){
-                                //     console.warn('write_completed', this, self);
-                                // });
-
-                                // pop.on('create_completed', self, function() {
-                                //     console.warn('create_completed', this, self);
-                                // });
-
-
-                            });
-
-                            self.$el.find('.oe_form_container [data-fieldname=' + column.id + ']').append(dialog)
-                        }
-
+                        };
                     };
                 })
             });
-        }
+        },
+        do_open_one2many_popup: function(options) {
+            var self = this,
+                field_column = false,
+                notify = instance.web.notification,
+                pop = new instance.web.form.FormOpenPopup(self);
+
+            if (_.isUndefined(options.open_dialog.field)) {
+                field_column = column;
+            } else {
+                field_column = _.find(self.columns, function(column) { return column.id === options.open_dialog.field});
+            }
+
+            if (_.isUndefined(field_column)) {
+                return notify.warn('错误', options.open_dialog.field + '字段需要在视图中定义');
+            }
+
+            if (_.isUndefined(options.open_dialog.view_id)) {
+                return notify.warn('错误', 'options中需要定义view_id来指定具体的视图');
+            }
+
+            var views = options.open_dialog.view_id.split('.');
+            if (views.length != 2) {
+                return notify.warn('错误', 'options中定义的视图id需要指定具体的模块名称');
+            }
+
+            new instance.web.Model('ir.model.data').call('get_object_reference', views).then(function(view_id) {
+                pop.show_element(
+                    self.model,
+                    false,
+                    self.dataset.get_context(),
+                    {
+                        view_id: view_id[1],
+                        create_function: function(data, options) {
+                            var item = _.find(self.fields_for_resize, function(item) {
+                                return item.field.name === field_column.id;
+                            });
+
+                            var history_value = item.field.get_value();
+                            item.field.set_value(history_value.concat(data[field_column.id]));
+
+                            pop.check_exit();
+                            return $.Deferred();
+                        }
+                    }
+                );
+            });
+        },
     });
 }
